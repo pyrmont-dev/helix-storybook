df0b8b76fafd8eba14cc9fbe9c6c4bb8a2219bf7
ewan.horsburgh <ewan.horsburgh@accenture.com>
2024-09-08 16:49:10 +1000

    Update button stories

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -7,13 +7,30 @@ export const Primary = {
     const btn = document.createElement('button');
     btn.innerText = args.label;
 
-    const mode = args.primary ? 'primary' : 'secondary';
-    btn.className = ['button-test', mode].join(' ');
+    // const mode = args.primary ? 'primary' : 'secondary';
+    const { variant } = args;
+    const { size } = args;
+    const { width } = args;
+    btn.className = ['button-test', variant, size, width].join(' ');
 
     return btn;
   },
   args: {
-    primary: true,
+    icon: true,
     label: 'Button',
+  },
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary', 'tertiary', 'muted'],
+      control: { type: 'radio' },
+    },
+    size: {
+      options: ['small', 'medium', 'large', 'xlarge'],
+      control: { type: 'radio' },
+    },
+    width: {
+      options: ['hug', 'full'],
+      control: { type: 'radio' },
+    },
   },
 };
